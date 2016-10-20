eab024f41e6e1f837a29f14281d092eb1e137418
somename <someemail>
2016-10-20 14:42:45 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -4,8 +4,18 @@
 // Finish time:
 
 public class PlanetExplorer {
-	public PlanetExplorer(int x, int y, String obstacles){
+	
+	int x; 
+	int y;
+
+	
+	public PlanetExplorer(int x, int y){
+		
+		this.x = x;
+		this.y = y;
+
 	/*	x and y represent the size of the grid.
+	 *
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
 	 *  
 		Example use: For a 100x100 grid with two obstacles at coordinates (5,5) and (7,8)
@@ -14,6 +24,29 @@ public class PlanetExplorer {
 	 */
 	}
 	
+	
+	
+	public int getX() {
+		return x;
+	}
+
+
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+
+
+	public int getY() {
+		return y;
+	}
+
+
+
+	public void setY(int y) {
+		this.y = y;
+	}
 	public String executeCommand(String command){
 		
 		/* The command string is composed of "f" (forward), "b" (backward), "l" (left) and "r" (right)
