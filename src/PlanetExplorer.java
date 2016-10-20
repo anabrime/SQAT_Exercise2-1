44b3b7a71d66a33728ee43f2247ac57459370519
somename <someemail>
2016-10-20 15:04:15 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -8,12 +8,17 @@ public class PlanetExplorer {
 	private int x; 
 	private int y;
 
+	private int explorerLands1;
+	private int explorerLands2;
+	private char explorerLands3;
 	
-	public PlanetExplorer(int x, int y){
+	public PlanetExplorer(int x, int y, int explorerLands1, int explorerLands2, char explorerLands3){
 		
 		this.x = x;
 		this.y = y;
-
+		this.explorerLands1 = 0;
+		this.explorerLands2 = 0;
+		this.explorerLands3 = 'N';
 	/*	x and y represent the size of the grid.
 	 *
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
@@ -53,6 +58,49 @@ public class PlanetExplorer {
 	public void setY(int y) {
 		this.y = y;
 	}
+	
+	public String getLandsOnThePlanet(){
+		
+		String journey;
+		journey  ="(" + getExplorerLands1() + "," + getExplorerLands2() + "," + getExplorerLands3() + ")";
+		return journey;
+	}
+	public int getExplorerLands1() {
+		return explorerLands1;
+	}
+
+
+
+	public void setExplorerLands1(int explorerLands1) {
+		this.explorerLands1 = explorerLands1;
+	}
+
+
+
+	public int getExplorerLands2() {
+		return explorerLands2;
+	}
+
+
+
+	public void setExplorerLands2(int explorerLands2) {
+		this.explorerLands2 = explorerLands2;
+	}
+
+
+
+	public char getExplorerLands3() {
+		return explorerLands3;
+	}
+
+
+
+	public void setExplorerLands3(char explorerLands3) {
+		this.explorerLands3 = explorerLands3;
+	}
+
+
+
 	public String executeCommand(String command){
 		
 		/* The command string is composed of "f" (forward), "b" (backward), "l" (left) and "r" (right)
